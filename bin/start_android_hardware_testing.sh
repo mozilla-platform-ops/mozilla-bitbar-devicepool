c3961347ee903547a0e25801c9614b1f88651bd9
Andrew Erickson <aerickson@mozilla.com>
2023-02-24 16:35:44 -0800

    change start script to use poetry

--- bin/start_android_hardware_testing.sh
+++ bin/start_android_hardware_testing.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 . /etc/bitbar/bitbar.env
-. /home/bitbar/mozilla-bitbar-devicepool/venv/bin/activate
-export PYTHONPATH=/home/bitbar/mozilla-bitbar-devicepool
-python3 /home/bitbar/mozilla-bitbar-devicepool/mozilla_bitbar_devicepool/main.py start-test-run-manager --update-bitbar
+
+# pre-poetry:
+# . /home/bitbar/mozilla-bitbar-devicepool/venv/bin/activate
+# export PYTHONPATH=/home/bitbar/mozilla-bitbar-devicepool
+# python3 /home/bitbar/mozilla-bitbar-devicepool/mozilla_bitbar_devicepool/main.py start-test-run-manager --update-bitbar
+
+# poetry:
+poetry run python3 /home/bitbar/mozilla-bitbar-devicepool/mozilla_bitbar_devicepool/main.py start-test-run-manager --update-bitbar
