c42c3bd4a30b14b04d7e7a6ba8f737d2b5f1632c
Andrew Erickson <aerickson@mozilla.com>
2023-03-01 15:52:45 -0800

    redo stop script

--- bin/stop_android_hardware_testing.sh
+++ bin/stop_android_hardware_testing.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-pkill -USR2 -f main.py
-while pkill -0 -f main.py; do
+# virtualenv
+# search_string="main.py"
+# poetry
+search_string="start-test-run-manager"
+
+pkill -USR2 -f "$search_string"
+while pkill -0 -f "$search_string"; do
     sleep 1
 done
