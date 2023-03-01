ea4f5dd66b33b4f5af7a25166ed0630efc81d2b1
Andrew Erickson <aerickson@mozilla.com>
2023-03-01 15:32:23 -0800

    redo start script

--- bin/start_android_hardware_testing.sh
+++ bin/start_android_hardware_testing.sh
@@ -2,10 +2,13 @@
 
 . /etc/bitbar/bitbar.env
 
-# pre-poetry:
+# old: virtualenv
+#
 # . /home/bitbar/mozilla-bitbar-devicepool/venv/bin/activate
 # export PYTHONPATH=/home/bitbar/mozilla-bitbar-devicepool
 # python3 /home/bitbar/mozilla-bitbar-devicepool/mozilla_bitbar_devicepool/main.py start-test-run-manager --update-bitbar
 
-# poetry:
-poetry run python3 /home/bitbar/mozilla-bitbar-devicepool/mozilla_bitbar_devicepool/main.py start-test-run-manager --update-bitbar
+# new: poetry
+#
+cd /home/bitbar/mozilla-bitbar-devicepool
+poetry run mbd start-test-run-manager --update-bitbar
