67300f741a453438054f4bf3b9f5b8a0db4195b0
Andrew Erickson <aerickson@mozilla.com>
2023-08-07 13:00:26 -0700

    docker server report (#58)

---
+++ mozilla_bitbar_devicepool/bitbar/admin_devices.py
@@ -0,0 +1,35 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from mozilla_bitbar_devicepool import TESTDROID
+from mozilla_bitbar_devicepool.util.template import get_filter
+
+
+# queries https://mozilla.bitbar.com/cloud/api/v2/admin/device/statuses
+#  e.g. https://mozilla.bitbar.com/cloud/api/v2/admin/device/statuses?sort=deviceName_a&offset=0&limit=10
+#
+# TODO: test if filter works
+def get_device_statuses(**kwargs):
+    """Return list of matching Bitbar device_groups belonging to current user.
+
+    :param **kwargs: keyword arguments containing fieldnames and
+                     values with which to filter the devices to
+                     be returned. If a fieldname is missing, it is
+                     not used in the filter.
+                     {
+                       'tbd': str,
+                       'tbd_int': int,
+                     }
+
+    Examples:
+       get_device_statuses() # Return all devices
+    """
+    fields = {"displayname": str, "id": int, "ostype": str}
+
+    # GET
+    filter = get_filter(fields, **kwargs)
+    response = TESTDROID.get(
+        "api/v2/admin/device/statuses", payload={"limit": 0, "filter": filter}
+    )
+    return response["data"]
---
+++ mozilla_bitbar_devicepool/docker_server_report.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sys
+import pprint
+
+import termplotlib as tpl
+
+from mozilla_bitbar_devicepool.bitbar.admin_devices import get_device_statuses
+
+
+class DockerServerReport:
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def do_report(self):
+        try:
+            results = get_device_statuses()
+        except AttributeError as e:
+            print(f"  exception: {e}")
+            print("Please source bitbar_env.sh (or your equivalent)!")
+            sys.exit(1)
+
+        device_to_docker_host_dict = {}
+        for result_device in results:
+            device_to_docker_host_dict[result_device["deviceName"]] = result_device[
+                "clusterName"
+            ]
+        if self.verbose:
+            pprint.pprint(device_to_docker_host_dict)
+            print("")
+
+        histogram_dict = {}
+        for k, v in device_to_docker_host_dict.items():
+            shortened_v = v.replace(".mv.mozilla.hc.bitbar", "")
+            try:
+                histogram_dict[shortened_v] += 1
+            except KeyError:
+                histogram_dict[shortened_v] = 1
+
+        sorted_dict = dict(sorted(histogram_dict.items()))
+
+        counts = list(sorted_dict.values())
+        labels = list(sorted_dict.keys())
+
+        fig = tpl.figure()
+        fig.barh(counts, labels, force_ascii=True)
+        fig.show()
+
+    def main(self):
+        self.do_report()
