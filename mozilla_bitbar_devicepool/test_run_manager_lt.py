9ad31e42a4fcacbec1f0c501183d0046b92a5fc9
Andrew Erickson <aerickson@mozilla.com>
2025-03-13 17:20:54 -0700

    add notes

--- mozilla_bitbar_devicepool/test_run_manager_lt.py
+++ mozilla_bitbar_devicepool/test_run_manager_lt.py
@@ -3,15 +3,19 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import signal
+import logging
+import time
 
 from mozilla_bitbar_devicepool import configuration_lt
 
 
 class TestRunManagerLT(object):
 
-    def __init__(self, wait=60):
-        self.wait = wait
+    def __init__(self, exit_wait=5):
+        self.exit_wait = exit_wait
         self.state = "RUNNING"
+        self.config_object = configuration_lt.ConfigurationLt()
+        self.config_object.configure()
 
         signal.signal(signal.SIGUSR2, self.handle_signal)
         signal.signal(signal.SIGINT, self.handle_signal)
@@ -22,21 +26,37 @@ class TestRunManagerLT(object):
 
         if signalnum == signal.SIGINT or signalnum == signal.SIGUSR2:
             self.state = "STOP"
+            # logging.info doesn't work in signal handlers
+            print(
+                f" handle_signal: set state to stop, exiting in {self.exit_wait} seconds or less"
+            )
 
     def run(self):
+
+        # overview:
+        #   1. do configuration / load config data
+        #   2. in loop:
+        #     a. update tc queue count for lt queues
+        #     b. update lt job status (how many running per each 'queue')
+        #     c. start jobs for the tc queue with the appropriate devices
+
         while self.state == "RUNNING":
+            # use logging to print 'running' with a datetime
+            logging.info("Running...")
+
             print("Running...")
-            # do something
 
             # `./hyperexecute –config hyperexecute.yaml
 
-            pass
+            if self.state == "STOP":
+                break
+            time.sleep(self.exit_wait)
+            if self.state == "STOP":
+                break
 
 
 if __name__ == "__main__":
-    # if main
-    # get configuration
-    config = configuration_lt.get_config()
-    # print configuration
-    print(config)
-    # end if main
+    # set logging levvel to info
+    logging.basicConfig(level=logging.INFO)
+    trmlt = TestRunManagerLT()
+    trmlt.run()
