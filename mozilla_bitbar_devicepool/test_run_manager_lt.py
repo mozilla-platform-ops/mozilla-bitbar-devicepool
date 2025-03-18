8c3682dcd50c1516e8c79ac4cfdbeb4288c21fe5
Andrew Erickson <aerickson@mozilla.com>
2025-03-17 20:14:27 -0700

    cleanup, work on subprocess signal handling blocking

--- mozilla_bitbar_devicepool/test_run_manager_lt.py
+++ mozilla_bitbar_devicepool/test_run_manager_lt.py
@@ -20,6 +20,8 @@ from mozilla_bitbar_devicepool.taskcluster import get_taskcluster_pending_tasks
 STOP = "000000001"
 RUNNING = "000000002"
 
+VERY_LARGE_NUMBER = 2000
+
 
 class TestRunManagerLT(object):
     def __init__(
@@ -49,7 +51,9 @@ class TestRunManagerLT(object):
             # for testing, exit immediately
             # sys.exit(0)
 
-    def run_single_project_single_thread_multi_job(self):
+    def run_single_project_single_thread_multi_job(
+        self, max_jobs_to_start=VERY_LARGE_NUMBER, foreground=False
+    ):
         # base on __file__ to get the project root dir
         project_source_dir = os.path.dirname(os.path.realpath(__file__))
         project_root_dir = os.path.abspath(os.path.join(project_source_dir, ".."))
@@ -117,13 +121,19 @@ class TestRunManagerLT(object):
                 # TODO: use env vars for setting user and key
                 # lt user and lt key are passsed in via env vars
                 # command_string = f"{project_root_dir}/hyperexecute --user '{self.config_object.lt_username}' --key '{self.config_object.lt_api_key}'"
-                command_string = f"{project_root_dir}/hyperexecute --no-track"
+                if foreground:
+                    command_string = f"{project_root_dir}/hyperexecute"
+                else:
+                    command_string = f"{project_root_dir}/hyperexecute --no-track"
 
                 cmd_env = os.environ.copy()
                 cmd_env["LT_USERNAME"] = self.config_object.lt_username
                 cmd_env["LT_ACCESS_KEY"] = self.config_object.lt_api_key
 
-                jobs_to_start = min(tc_job_count, self.max_jobs_to_start)
+                #
+                jobs_to_start = min(
+                    tc_job_count, self.max_jobs_to_start, max_jobs_to_start
+                )
                 for i in range(jobs_to_start):
                     logging.info(f"starting job {i + 1} of {jobs_to_start}...")
                     # Use test_mode instead of hardcoded DEBUG
@@ -136,109 +146,21 @@ class TestRunManagerLT(object):
                             f"running: '{command_string}' in path '{test_run_dir}'..."
                         )
                         start_time = time.time()
-                        # TODO: background so we can do this faster
+                        # TODO: background so we can do this faster or set concurrencty in the YAML?
                         #   - can only get 1-2 jobs going in parallel with trivial tasks
+                        #   - for concurrency to work, would we need to emit multiple test targets (1:1 with concurrency?)
+                        # start_new_session=True ensures process ignores ctrl-c sent to this process (so it cleans up)
                         subprocess.run(
-                            command_string, shell=True, env=cmd_env, cwd=test_run_dir
+                            command_string,
+                            shell=True,
+                            env=cmd_env,
+                            cwd=test_run_dir,
+                            start_new_session=True,
                         )
                         end_time = time.time()
                         logging.info(
                             f"starting job {i + 1} of {jobs_to_start} took {end_time - start_time} seconds"
                         )
-
-            if self.state == STOP:
-                break
-            time.sleep(self.exit_wait)
-            if self.state == STOP:
-                break
-
-    def run_single_project_single_thread_single_job(self):
-        # base on __file__ to get the project root dir
-        project_source_dir = os.path.dirname(os.path.realpath(__file__))
-        project_root_dir = os.path.abspath(os.path.join(project_source_dir, ".."))
-        user_script_golden_dir = os.path.join(
-            project_source_dir, "lambdatest", "user_script"
-        )
-
-        test_run_dir = "/tmp/mozilla-lt-devicepool-job-dir"
-        test_run_file = os.path.join(test_run_dir, "hyperexecute.yaml")
-
-        # overview:
-        #   1. do configuration / load config data
-        #   2. in loop:
-        #     a. update tc queue count for lt queues
-        #     b. update lt job status (how many running per each 'queue')
-        #     c. start jobs for the tc queue with the appropriate devices
-
-        # TODO: if we do multithreading, use messaging vs shared object with locks
-
-        logging.info("entering run loop...")
-        while self.state == RUNNING:
-            # only a single project for now, so load that up
-            current_project = self.config_object.config["projects"]["a55-alpha"]
-
-            # TODO: check tc and if there are jobs, continue, exist go to sleep
-            tc_job_count = get_taskcluster_pending_tasks(
-                "proj-autophone", current_project["TC_WORKER_TYPE"], verbose=False
-            )
-            logging.info(f"tc_job_count: {tc_job_count}")
-            if tc_job_count <= 0:
-                logging.info(f"no jobs found, sleeping {self.no_job_sleep}s...")
-                time.sleep(self.no_job_sleep)
-            else:
-                # tc_worker_type = current_project["TC_WORKER_TYPE"]
-                tc_client_id = current_project["TASKCLUSTER_CLIENT_ID"]
-                tc_client_key = current_project["TASKCLUSTER_ACCESS_TOKEN"]
-                # debug
-                # print(f"tc_client_id: {tc_client_id}")
-                # print(f"tc_client_key: {tc_client_key}")
-
-                # TODO: verify this apk is ok, update the apk if needed, and store/use it's app_id
-                lt_app_url = "lt://APP1016023521741987818221818"
-
-                # remove /tmp/user_script dir
-                shutil.rmtree(test_run_dir, ignore_errors=True)
-                # create /tmp/mozilla-lt-devicepool-job-dir dir
-                os.makedirs(test_run_dir, exist_ok=True)
-
-                # TODO: create hyperexecute.yaml specific to each queue
-                job_config.write_config(
-                    tc_client_id,
-                    tc_client_key,
-                    lt_app_url,
-                    test_run_file,
-                    concurrency=1,
-                )
-
-                # TODO: copy user-script dir to correct dir (use basename on config_file_path?)
-                # copy user_script_golden_dir to correct path using python shutil
-                shutil.copytree(
-                    user_script_golden_dir, os.path.join(test_run_dir, "user_script")
-                )
-
-                # TODO: loop the number of jobs we need
-                # TODO: use env vars for setting user and key
-                # lt user and lt key are passsed in via env vars
-                # command_string = f"{project_root_dir}/hyperexecute --user '{self.config_object.lt_username}' --key '{self.config_object.lt_api_key}'"
-                command_string = f"{project_root_dir}/hyperexecute"
-
-                cmd_env = os.environ.copy()
-                cmd_env["LT_USERNAME"] = self.config_object.lt_username
-                cmd_env["LT_ACCESS_KEY"] = self.config_object.lt_api_key
-
-                # Use test_mode instead of hardcoded DEBUG
-                if self.test_mode:
-                    logging.info(
-                        f"would be running command: '{command_string}' in path '{test_run_dir}'..."
-                    )
-                else:
-                    logging.info(
-                        f"running: '{command_string}' in path '{test_run_dir}'..."
-                    )
-                    # TODO: use --no-track or figure out how to make subprocess interactive so it doesn't die
-                    subprocess.run(
-                        command_string, shell=True, env=cmd_env, cwd=test_run_dir
-                    )
 
             if self.state == STOP:
                 break
@@ -272,5 +194,7 @@ if __name__ == "__main__":
     # pprint.pprint(trmlt.config_object.config)
 
     # start the main run loop
-    # trmlt.run_single_project_single_thread_single_job()
-    trmlt.run_single_project_single_thread_multi_job()
+    # single job started at a time
+    # trmlt.run_single_project_single_thread_multi_job(max_jobs_to_start=1, foreground=True)
+    # multiple jobs started in background
+    trmlt.run_single_project_single_thread_multi_job()  # max_jobs_to_start=5, foreground=False)
