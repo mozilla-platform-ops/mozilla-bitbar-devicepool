99148a03f77e059bc498a460fe573c1d83d70362
Andrew Erickson <aerickson@mozilla.com>
2025-03-19 14:11:15 -0700

    working on api calls, todo

--- mozilla_bitbar_devicepool/lambdatest/status.py
+++ mozilla_bitbar_devicepool/lambdatest/status.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import sys
 import pprint
 
 from mozilla_bitbar_devicepool.lambdatest.api import get_jobs, get_devices
@@ -19,20 +20,49 @@ class Status:
 
     def get_job_dict(self, jobs=100):
         jobs = get_jobs(self.lt_username, self.lt_api_key, jobs=jobs)
-        # pprint.pprint(jobs)
+        pprint.pprint(jobs)
         job_result_dict = {}
         for job in jobs["data"]:
             job_result_dict[job["job_number"]] = job["status"]
         # TODO: return sorted?
         return job_result_dict
 
+    # format:
+    # {
+    #   'running': 12,
+    #   'aborted': 5,
+    #   ...
+    # }
+    #
+    # possible states:
+    #  - initiated
+    #  - running
+    #  - completed
+    #  - aborted
+    #  - failed
+    #  - timeout
+    #
+    def get_job_summary(self, jobs=100):
+        gj_output = get_jobs(self.lt_username, self.lt_api_key, jobs=jobs)
+        result_dict = {}
+        for job in gj_output["data"]:
+            status = job["status"]
+            if status in result_dict:
+                result_dict[status] += 1
+            else:
+                result_dict[status] = 1
+        return result_dict
+
     def get_initiated_job_count(self, jobs=100):
-        jobs = get_jobs(self.lt_username, self.lt_api_key, jobs=jobs)
+        gj_output = get_jobs(self.lt_username, self.lt_api_key, jobs=jobs)
         initiated_job_count = 0
-        for job in jobs["data"]:
+        for job in gj_output["data"]:
             if job["status"] == "initiated":
                 initiated_job_count += 1
         return initiated_job_count
+
+    # TODO: find out which job is for which 'workerType' otherwise we can only run one workerType...
+    #   - use job label? `--labels` arg to hyperexecute
 
     # devices
 
@@ -65,7 +95,16 @@ if __name__ == "__main__":
 
     status = Status(lt_username, lt_api_key)
 
-    # pprint.pprint(status.get_job_dict())
-    # print(f"initiated job count: {status.get_initiated_job_count()}")
+    pprint.pprint(status.get_job_dict())
+    print("")
+    sys.exit(0)
+    print(f"initiated job count: {status.get_initiated_job_count()}")
+    print("job summary:")
+    pprint.pprint(status.get_job_summary())
 
+    print("")
+
+    print("device list:")
     pprint.pprint(status.get_device_list())
+
+    sys.exit(0)
