2abc7f570fc1ab94ad60b3acf0a4cea0ef5bac71
Andrew Erickson <aerickson@mozilla.com>
2025-04-08 16:42:11 -0400

    bug fix

--- bin/start_ltdp.sh
+++ bin/start_ltdp.sh
@@ -2,5 +2,5 @@
 
 . /etc/bitbar/lambdatest.env
 
-cd /home/bitbar/mozilla-taskcluster-devicepool
+cd /home/bitbar/mozilla-bitbar-devicepool
 /home/bitbar/.local/bin/poetry run mld start-test-run-manager
