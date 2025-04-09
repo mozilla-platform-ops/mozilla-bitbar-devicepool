96e16406713a24f306998fd1d2e593470d71899a
Andrew Erickson <aerickson@mozilla.com>
2025-04-09 00:05:32 -0400

    tc api code: use HTTPAdapter to retry w/ exponential backoff

--- mozilla_bitbar_devicepool/taskcluster.py
+++ mozilla_bitbar_devicepool/taskcluster.py
@@ -3,16 +3,39 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 
 
 def get_taskcluster_pending_tasks(provisioner_id, worker_type, verbose=False):
+    # define the retry strategy
+    retry_strategy = Retry(
+        total=4,  # maximum number of retries
+        backoff_factor=2,
+        status_forcelist=[
+            429,
+            500,
+            502,
+            503,
+            504,
+        ],  # the HTTP status codes to retry on
+    )
+
+    # create an HTTP adapter with the retry strategy and mount it to the session
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+
+    # create a new session object
+    session = requests.Session()
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+
     taskcluster_queue_url = (
         "https://firefox-ci-tc.services.mozilla.com/api/queue/v1/pending/%s/%s"
         % (provisioner_id, worker_type)
     )
     if verbose:
         print("taskcluster_queue_url: %s" % taskcluster_queue_url)
-    r = requests.get(taskcluster_queue_url)
+    r = session.get(taskcluster_queue_url)
     if verbose:
         print("r.status_code: %s" % r.status_code)
         print("r.text: %s" % r.text if r.text else "r.content: %s" % r.content)
