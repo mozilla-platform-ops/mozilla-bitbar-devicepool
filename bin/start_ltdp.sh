a0d58792351b78018ade6b66b038aeeab8585338
Andrew Erickson <aerickson@mozilla.com>
2025-05-01 21:47:31 -0700

    tweak start command

--- bin/start_ltdp.sh
+++ bin/start_ltdp.sh
@@ -3,4 +3,9 @@
 . /etc/bitbar/lambdatest.env
 
 cd /home/bitbar/mozilla-bitbar-devicepool
-/home/bitbar/.local/bin/poetry run mld start-test-run-manager --log-level DEBUG
+
+# debugging
+# /home/bitbar/.local/bin/poetry run mld start-test-run-manager --log-level DEBUG
+
+# normal
+/home/bitbar/.local/bin/poetry run mld start-test-run-manager
