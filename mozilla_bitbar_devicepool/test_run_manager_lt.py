5e363d93568432b340f0edc80e5dcf558610d4c5
Andrew Erickson <aerickson@mozilla.com>
2025-05-02 23:55:20 -0700

    rename var

--- mozilla_bitbar_devicepool/test_run_manager_lt.py
+++ mozilla_bitbar_devicepool/test_run_manager_lt.py
@@ -192,8 +192,8 @@ class TestRunManagerLT(object):
                 except Exception as e:
                     logging.error(f"{logging_header} Error fetching TC tasks for {project_name}: {e}", exc_info=True)
 
-            formatted_string = str(worker_type_to_count_dict).strip("{}").replace("'", "")
-            logging.info(f"{logging_header} Queue counts: {formatted_string}")
+            formatted_wttcd = str(worker_type_to_count_dict).strip("{}").replace("'", "")
+            logging.info(f"{logging_header} Queue counts: {formatted_wttcd}")
 
             # Wait for the specified interval or until shutdown is signaled
             self.shutdown_event.wait(self.TC_MONITOR_INTERVAL)
