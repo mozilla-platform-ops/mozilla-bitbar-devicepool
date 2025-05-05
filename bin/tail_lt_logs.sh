66764fad631347b9289e8d5dc7251387736f7394
Andrew Erickson <aerickson@mozilla.com>
2025-05-05 15:06:55 -0700

    more tail script work

--- bin/tail_lt_logs.sh
+++ bin/tail_lt_logs.sh
@@ -9,15 +9,24 @@ set -x
 # wider, more color
 #journalctl -u lambdatest -f -n 20000 | grep 'a55-perf' | GREP_COLOR='1;32' grep --color=always -E '.*Launched.*|^' | GREP_COLOR='1;33' grep --color=always -E '.*Monitor.*|^'
 
+GREP_COLOR_RED='1;31'
+GREP_COLOR_GREEN='1;32'
+GREP_COLOR_YELLOW='1;33'
+GREP_COLOR_BLUE='1;34'
+GREP_COLOR_PURPLE='1;35'
+GREP_COLOR_CYAN='1;36'
+GREP_COLOR_WHITE='1;37'
+
 # even wider
 LINES_TO_SHOW=1000
+GREP_OPTIONS='--line-buffered'
 #journalctl -u lambdatest -f -n "${LINES_TO_SHOW}" | grep 'a55-perf' | \
 #journalctl -u lambdatest -f -n "${LINES_TO_SHOW}" | grep -E 'Monitor|a55-perf' | \
 #journalctl -u lambdatest -f -n "${LINES_TO_SHOW}" | \
         #GREP_COLOR='1;36' grep --color=always -E '.* TC Jobs.*|^' | \
 journalctl -u lambdatest -f -n "${LINES_TO_SHOW}" | \
         grep --line-buffered -v DEBUG | \
-        GREP_COLOR='1;32' grep --line-buffered --color=always -E '.*Launched.*|^' | \
-        GREP_COLOR='1;35' grep --line-buffered --color=always -E '.*LT Monitor.*|^' | \
-        GREP_COLOR='1;34' grep --line-buffered --color=always -E '.*TC Monitor.*|^' | \
-        GREP_COLOR='1;31' grep --line-buffered --color=always -E '.*WARNING.*|^'
+        GREP_COLOR=$GREP_COLOR_GREEN grep ${GREP_OPTIONS} --color=always -E '.*Launched.*|^' | \
+        GREP_COLOR=$GREP_COLOR_PURPLE grep ${GREP_OPTIONS} --color=always -E '.*LT Monitor.*|^' | \
+        GREP_COLOR=$GREP_COLOR_BLUE grep ${GREP_OPTIONS} --color=always -E '.*TC Monitor.*|^' | \
+        GREP_COLOR=$GREP_COLOR_RED grep ${GREP_OPTIONS} --color=always -E '.*WARNING.*|^'
