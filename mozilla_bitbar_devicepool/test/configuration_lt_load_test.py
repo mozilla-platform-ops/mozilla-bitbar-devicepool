b532797b7ed28310b90207c541fda46405f0be54
Andrew Erickson <aerickson@mozilla.com>
2025-05-05 16:56:35 -0700

    better load test data

--- mozilla_bitbar_devicepool/test/configuration_lt_load_test.py
+++ mozilla_bitbar_devicepool/test/configuration_lt_load_test.py
@@ -7,26 +7,116 @@ from mozilla_bitbar_devicepool.configuration_lt import ConfigurationLt
 
 # Sample configuration data as a dictionary (for assertion)
 SAMPLE_FILE_CONFIG_DICT = {
-    "projects": {
-        "defaults": {"default_key": "default_value"},
-        "projectC": {"specific_key": "specific_value"},
+    "device_groups": {"projectC": "udid5 udid6"},
+    "projects": {"defaults": {"default_key": "default_value"}, "projectC": {"specific_key": "specific_value"}},
+    "device_groups": {  # noqa: F601
+        "a55-alpha": "R5CXC1HZKLR",
+        "a55-perf": "R5CX4089QNL R5CXC1AHV4M R5CXC1ALFED "
+        "R5CXC1AMMNK R5CXC1AMNFY R5CXC1ANGLT "
+        "R5CXC1AP2KT R5CXC1ARCER R5CXC1ARELR "
+        "R5CXC1ARM0A R5CXC1ASA0L R5CXC1ASA2E "
+        "R5CXC1ASA3P R5CXC1ASLHH R5CXC1HZK0W "
+        "R5CY128X71B R5CY21T22NH RZCX31FDGJE "
+        "RZCX50TW03H RZCX71ZVF6J RZCX821GXDJ "
+        "RZCX821GYPX RZCXA0H3T9P RZCY10LGB6W "
+        "RZCY10Y4HWD RZCY10Y4QVX RZCY10Y4TAV "
+        "RZCY10Y4TBY RZCY10Y4TJX RZCY10Y548K "
+        "RZCY2011M7N RZCY203N75Z RZCY204AAZD",
+        "test-1": "RZ8NB0WJ47H",
     },
-    "device_groups": {
-        "projectC": "udid5 udid6",
+    "global": {"contract_device_count": 30},
+    "projects": {  # noqa: F601
+        "a55-alpha": {
+            "TASKCLUSTER_CLIENT_ID": "project/autophone/gecko-t-lambda-alpha-a55",
+            "TC_WORKER_TYPE": "gecko-t-lambda-alpha-a55",
+            "lt_device_selector": "Galaxy A51-11",
+        },
+        "a55-perf": {
+            "TASKCLUSTER_CLIENT_ID": "project/autophone/gecko-t-lambda-perf-a55",
+            "TC_WORKER_TYPE": "gecko-t-lambda-perf-a55",
+            "lt_device_selector": "Galaxy A55 5G-14",
+        },
+        "defaults": {
+            "SCRIPT_REPO_COMMIT": "master",
+            "TASKCLUSTER_CLIENT_ID": "tc-client-id-set-me",
+            "TC_WORKER_TYPE": "tc-worker-type-set-me",
+            "lt_device_selector": "Samsung GZ4200-90",
+        },
     },
-    "some_other_top_level_key": "value123",
 }
 
 # Sample configuration data as a raw YAML string (for writing to file)
 SAMPLE_FILE_CONFIG_YAML = """
+global:
+  contract_device_count: 30
 projects:
   defaults:
-    default_key: default_value
-  projectC:
-    specific_key: specific_value
+    SCRIPT_REPO_COMMIT: master
+    # fake values. inheriting projects below should set reasonable values.
+    lt_device_selector: "Samsung GZ4200-90"
+    TASKCLUSTER_CLIENT_ID: tc-client-id-set-me
+    # env var with key is looked for at this with underscores: `tc_worker_type_set_me`
+    TC_WORKER_TYPE: tc-worker-type-set-me
+  a55-alpha:
+    # lt_device_selector: "Galaxy A55 5G-14"
+    # swapped for testing
+    lt_device_selector: "Galaxy A51-11"
+    TASKCLUSTER_CLIENT_ID: project/autophone/gecko-t-lambda-alpha-a55
+    TC_WORKER_TYPE: gecko-t-lambda-alpha-a55
+  a55-perf:
+    lt_device_selector: "Galaxy A55 5G-14"
+    # swapped for testing
+    # lt_device_selector: "Galaxy A51-11"
+    TASKCLUSTER_CLIENT_ID: project/autophone/gecko-t-lambda-perf-a55
+    TC_WORKER_TYPE: gecko-t-lambda-perf-a55
+  # test-1:
+  #     SCRIPT_REPO_COMMIT: future_commit
+  #     lt_device_selector: "Galaxy A51-11"
+  #     TASKCLUSTER_CLIENT_ID: project/autophone/gecko-t-lambda-test-1
+  #     TC_WORKER_TYPE: gecko-t-lambda-gw-test-1
+  #     # override SCRIPT_REPO_COMMIT with a test commit
 device_groups:
-  projectC: udid5 udid6
-some_other_top_level_key: value123
+  # this block is not used yet (not possible with LT API), future goal. see lt_device_selector in projects.
+  a55-perf:
+    R5CX4089QNL
+    R5CXC1AHV4M
+    R5CXC1ALFED
+    R5CXC1AMMNK
+    R5CXC1AMNFY
+    R5CXC1ANGLT
+    R5CXC1AP2KT
+    R5CXC1ARCER
+    R5CXC1ARELR
+    R5CXC1ARM0A
+    R5CXC1ASA0L
+    R5CXC1ASA2E
+    R5CXC1ASA3P
+    R5CXC1ASLHH
+    R5CXC1HZK0W
+    R5CY128X71B
+    R5CY21T22NH
+    RZCX31FDGJE
+    RZCX50TW03H
+    RZCX71ZVF6J
+    RZCX821GXDJ
+    RZCX821GYPX
+    RZCXA0H3T9P
+    RZCY10LGB6W
+    RZCY10Y4HWD
+    RZCY10Y4QVX
+    RZCY10Y4TAV
+    RZCY10Y4TBY
+    RZCY10Y4TJX
+    RZCY10Y548K
+    RZCY2011M7N
+    RZCY203N75Z
+    RZCY204AAZD
+  a55-alpha:
+    # the only device with a power meter
+    R5CXC1HZKLR
+  test-1:
+    # a51
+    RZ8NB0WJ47H
 """
 
 
