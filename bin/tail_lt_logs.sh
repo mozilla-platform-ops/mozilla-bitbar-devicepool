522b3a5f20ef350861e845c5bba60cffc52b46f5
Andrew Erickson <aerickson@mozilla.com>
2025-05-05 09:11:21 -0700

    add tail script

---
+++ bin/tail_lt_logs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+#set -x
+
+# more restricted
+#journalctl -u lambdatest -f -n 20000 | grep -E 'Launched|TC Jobs' | grep 'a55-perf' | GREP_COLOR='1;32' grep --color=always -E '.*Launched.*|^'
+
+# wider, more color
+#journalctl -u lambdatest -f -n 20000 | grep 'a55-perf' | GREP_COLOR='1;32' grep --color=always -E '.*Launched.*|^' | GREP_COLOR='1;33' grep --color=always -E '.*Monitor.*|^'
+
+# even wider
+LINES_TO_SHOW=5000
+#journalctl -u lambdatest -f -n "${LINES_TO_SHOW}" | grep 'a55-perf' | \
+journalctl -u lambdatest -f -n "${LINES_TO_SHOW}" | grep -E 'Monitor|a55-perf' | \
+        GREP_COLOR='1;32' grep --color=always -E '.*Launched.*|^' | \
+        GREP_COLOR='1;35' grep --color=always -E '.*LT Monitor.*|^' | \
+        GREP_COLOR='1;34' grep --color=always -E '.*TC Monitor.*|^' | \
+        GREP_COLOR='1;31' grep --color=always -E '.*Direct.*|^'
