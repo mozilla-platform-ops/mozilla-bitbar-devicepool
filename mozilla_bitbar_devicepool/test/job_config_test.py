8e06738f07b2b231fb0aeac0581de4b8d9589430
Andrew Erickson <aerickson@mozilla.com>
2025-05-08 20:12:40 -0700

    job_config: add test

--- mozilla_bitbar_devicepool/test/job_config_test.py
+++ mozilla_bitbar_devicepool/test/job_config_test.py
@@ -35,9 +35,36 @@ def test_return_config():
     # TODO: add more
 
 
-# # test write_config
-# def test_write_config():
-#     # test with all parameters
-#     config = job_config.write_config(
-#         tc_client_id="test_client_id",
-#         tc_access_token="test_access_token",
+# test write_config
+def test_write_config(tmp_path):
+    # d = tmp_path / "sub"
+    # d.mkdir()
+    p = tmp_path / "he.yml"
+    # p.write_text(CONTENT)
+
+    #
+    tc_client_id = "test_client_id"
+    tc_access_token = "test_access_token"
+    tc_worker_type = "test_worker_type"
+    lt_app_url = "https://example.com"
+    device_type_and_os = "Galaxy A55 5G-14"
+    udid = "test_udid"
+    concurrency = 918
+    # test with all parameters
+    _blah = job_config.write_config(
+        tc_client_id, tc_access_token, tc_worker_type, lt_app_url, device_type_and_os, udid, concurrency, path=p
+    )
+    # open the file and check contents
+    with open(p, "r") as f:
+        config = f.read()
+    # basic checks
+    assert config is not None
+    assert udid in config
+    assert lt_app_url in config
+    assert tc_client_id in config
+    assert tc_access_token in config
+    assert tc_worker_type in config
+    assert device_type_and_os in config
+    # more complex checks
+    assert f'fixedIP: "{udid}"' in config
+    assert f"concurrency: {concurrency}" in config
