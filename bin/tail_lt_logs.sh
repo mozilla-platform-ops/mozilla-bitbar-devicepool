86add60f30300f03fd13e446bb8e980b48f99175
Andrew Erickson <aerickson@mozilla.com>
2025-05-09 22:15:53 -0700

    tail tweak 3333

--- bin/tail_lt_logs.sh
+++ bin/tail_lt_logs.sh
@@ -31,7 +31,7 @@ journalctl -u lambdatest -f -n "${LINES_TO_SHOW}" | \
         grep --line-buffered -v DEBUG | \
         GREP_COLOR=$GREP_COLOR_RED grep ${GREP_OPTIONS} --color=always -E '.*WARNING.*|^' | \
         GREP_COLOR=$GREP_COLOR_YELLOW grep ${GREP_OPTIONS} --color=always -E '.*Main.*|^' | \
-        GREP_COLOR=$GREP_COLOR_CYAN grep ${GREP_OPTIONS} --color=always -E '.*Reporting.*|^' | \
+        GREP_COLOR=$GREP_COLOR_CYAN grep ${GREP_OPTIONS} --color=always -E '.*Monitor.*|^' | \
         GREP_COLOR=$GREP_COLOR_GREEN grep ${GREP_OPTIONS} --color=always -E '.*Launched.*|^' | \
         GREP_COLOR=$GREP_COLOR_BLUE grep ${GREP_OPTIONS} --color=always -E '.*LT API.*|^' | \
         GREP_COLOR=$GREP_COLOR_PURPLE grep ${GREP_OPTIONS} --color=always -E '.*TC API.*|^'
