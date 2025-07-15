023b5105644f69ac581ae1d8e5e28cfbe066a487
Andrew Erickson <aerickson@mozilla.com>
2025-07-14 20:26:08 -0700

    add cli for new tool

--- mozilla_bitbar_devicepool/lambdatest/status.py
+++ mozilla_bitbar_devicepool/lambdatest/status.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
 import pprint
 
 import mozilla_bitbar_devicepool.lambdatest.util as util
