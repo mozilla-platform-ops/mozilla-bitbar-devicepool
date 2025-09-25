ad4cd79be8519e393ad122826f556f369cf8f309
Andrew Erickson <aerickson@mozilla.com>
2025-09-25 14:55:22 -0700

    logging setup: rework

--- mozilla_bitbar_devicepool/logging_setup.py
+++ mozilla_bitbar_devicepool/logging_setup.py
@@ -12,24 +12,36 @@ def setup_logging(level=logging.INFO, disable_timestamps=False):
     # TODO: make -vv show these or something
     #
     # Get the logger for 'requests' (and others) and set its level to INFO
-    # logging.getLogger("requests").setLevel(logging.INFO)
-    logging.getLogger("requests_cache").setLevel(logging.WARNING)
-    # logging.getLogger("urllib3").setLevel(logging.INFO)
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
-    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.getLogger("requests").setLevel(logging.INFO)
+    logging.getLogger("requests_cache").setLevel(logging.INFO)
+    logging.getLogger("urllib3").setLevel(logging.INFO)
+
+    # hide logging for things we don't want to see
+    logging.getLogger("mohawk").setLevel(logging.WARNING)
     logging.getLogger("taskcluster").setLevel(logging.WARNING)
+
+    # enable for debugging the source of messages
+    include_name = False
+
+    # inject name into format if include_name is True
+    if include_name:
+        format_string = "%(name)s - %(levelname)s - %(message)s"
+        format_string_with_time = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    else:
+        format_string = "%(levelname)s - %(message)s"
+        format_string_with_time = "%(asctime)s - %(levelname)s - %(message)s"
 
     if disable_timestamps:
         # Disable timestamps in the log messages
         logging.basicConfig(
             level=level,
-            format="%(levelname)s - %(message)s",
+            format=format_string,
             force=True,  # Force reconfiguration of the root logger
         )
     else:
         logging.basicConfig(
             level=level,
-            format="%(asctime)s - %(levelname)s - %(message)s",
+            format=format_string_with_time,
             datefmt="%Y-%m-%d %H:%M:%S",
             force=True,  # Force reconfiguration of the root logger
         )
