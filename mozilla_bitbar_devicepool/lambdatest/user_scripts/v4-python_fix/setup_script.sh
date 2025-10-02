cbc40e52ff96ce02f8460f86fc6bd35f3286bcde
Andrew Erickson <aerickson@mozilla.com>
2025-10-01 20:01:33 -0700

    lt user scripts v4: add two missing packages

--- mozilla_bitbar_devicepool/lambdatest/user_scripts/v4-python_fix/setup_script.sh
+++ mozilla_bitbar_devicepool/lambdatest/user_scripts/v4-python_fix/setup_script.sh
@@ -72,6 +72,9 @@ wget https://android-packages.s3.us-west-2.amazonaws.com/python3.12_for_20.04/py
 wget https://android-packages.s3.us-west-2.amazonaws.com/python3.12_for_20.04/python3.12-dev_3.12.11-16-gade85bc5f4-1%2Bfocal1_amd64.deb
 wget https://android-packages.s3.us-west-2.amazonaws.com/python3.12_for_20.04/python3.12-lib2to3_3.12.11-16-gade85bc5f4-1%2Bfocal1_all.deb
 wget https://android-packages.s3.us-west-2.amazonaws.com/python3.12_for_20.04/python3.12-venv_3.12.11-16-gade85bc5f4-1%2Bfocal1_amd64.deb
+wget https://android-packages.s3.us-west-2.amazonaws.com/python3.12_for_20.04/libpython3.12_3.12.11-16-gade85bc5f4-1%2Bfocal1_amd64.deb
+wget https://android-packages.s3.us-west-2.amazonaws.com/python3.12_for_20.04/libpython3.12-stdlib_3.12.11-16-gade85bc5f4-1%2Bfocal1_amd64.deb
+# now install
 sudo dpkg -i ./*.deb
 cd -
 
