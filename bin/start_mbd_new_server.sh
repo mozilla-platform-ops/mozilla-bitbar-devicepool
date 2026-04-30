592af94eb8ba96b26f7013e2ae2b1c1925117e4c
Andrew Erickson <aerickson@mozilla.com>
2026-04-29 19:39:02 -0700

    new server start script: fix

--- bin/start_mbd_new_server.sh
+++ bin/start_mbd_new_server.sh
@@ -4,4 +4,7 @@ set -e
 set -x
 
 . ./bitbar_env-v3-server.sh
-poetry run mbd start-test-run-manager -b config/config-v3-server.yml --update-bitbar
+
+
+cd /home/bitbar/mozilla-bitbar-devicepool
+/home/bitbar/.local/bin/poetry run mbd start-test-run-manager -b config/config-v3-server.yml --update-bitbar
